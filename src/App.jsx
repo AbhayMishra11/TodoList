424c1d2222597e7e38cae53e1b9c343d33fdcb97
AbhayMishra11 <55750241+AbhayMishra11@users.noreply.github.com>
2024-10-18 13:39:06 +0530

    adding event

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Navbar from './components/Navbar'
 
 function App() {
+  
+  const handleAdd=()=>{
+
+  } 
+
+  const handleEdit=()=>{
+
+  }
+
+  const handleDelete=()=>{
+
+  }
 
   return (
     <>
@@ -10,15 +22,15 @@ function App() {
         <div className="addTodo my-5">
           <h2 className='text-lg font-bold'>Add a Todo</h2>
           <input type="text" className='w-2/5' />
-          <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-6'>Add</button>
+          <button onClick={handleAdd} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-6'>Add</button>
         </div>
         <h2 className='text-lg font-bold'>My Tasks</h2>
         <div className="todos">
           <div className="todo flex">
             <div className="text">Lorem ipsum dolor sit amet.</div>
             <div className="buttons">
-              <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-1'>Edit</button>
-              <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-1'>Remove</button>
+              <button onClick={handleEdit} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-1'>Edit</button>
+              <button onClick={handleDelete} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-1'>Remove</button>
             </div>
           </div>
         </div>
