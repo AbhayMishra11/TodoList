ab90f7ad6179e9557f23c8678fd164bab71fcbc8
AbhayMishra11 <55750241+AbhayMishra11@users.noreply.github.com>
2024-10-18 14:16:21 +0530

    adding js to add button

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useState,useEffect } from 'react'
 import Navbar from './components/Navbar'
 
 function App() {
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
   
   const handleAdd=()=>{
-
+    setTodos([...todos,{todo,isCompleted: false}])
+    setTodo("")
   } 
 
   const handleEdit=()=>{
@@ -14,6 +17,9 @@ function App() {
   const handleDelete=()=>{
 
   }
+  const handleChange=(e)=>{
+     setTodo(e.target.value)
+  }
 
   return (
     <>
@@ -21,18 +27,20 @@ function App() {
       <div className='container mx-auto bg-violet-100 rounded-xl my-5 p-5 min-h-[80vh]'>
         <div className="addTodo my-5">
           <h2 className='text-lg font-bold'>Add a Todo</h2>
-          <input type="text" className='w-2/5' />
+          <input type="text" className='w-2/5' onChange={handleChange} value={todo} />
           <button onClick={handleAdd} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-6'>Add</button>
         </div>
         <h2 className='text-lg font-bold'>My Tasks</h2>
         <div className="todos">
-          <div className="todo flex">
-            <div className="text">Lorem ipsum dolor sit amet.</div>
+          {todos.map(items=>{
+          return <div className="todo flex">
+            <div >{items.todo}</div>
             <div className="buttons">
               <button onClick={handleEdit} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-1'>Edit</button>
               <button onClick={handleDelete} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-1'>Remove</button>
             </div>
           </div>
+          })}
         </div>
       </div>
     </>
