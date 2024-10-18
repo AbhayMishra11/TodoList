14d9ed3f133101e3c4bf8b7e345fbb51aa7b1a41
AbhayMishra11 <55750241+AbhayMishra11@users.noreply.github.com>
2024-10-18 12:26:09 +0530

    improving navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="Navbar flex justify-around  p-5 bg-slate-500 text-white">
-        <div className="logo">Let's Go</div>
-        <ul className='flex gap-[20px]'>
-            <li className='list-none'>Home</li>
-            <li className='list-none'>About</li>
-        </ul>
+    <nav className="Navbar flex justify-between  p-5 bg-slate-800 text-white">
+      <div className="logo font-bold mx-9 text-xl">Let's Go</div>
+      <ul className='flex gap-[20px] mx-9'>
+        <li className='cursor-pointer p-2 text-lg hover:font-bold transition-all duration-400'>Home</li>
+        <li className='cursor-pointer p-2 text-lg hover:font-bold  transition-all duration-400'>My Tasks</li>
+      </ul>
     </nav>
   )
 }
