5aeec5cb08f3771ba42f626e82579d2ecd6e44fb
AbhayMishra11 <55750241+AbhayMishra11@users.noreply.github.com>
2024-10-18 13:27:53 +0530

    Adding basic html structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,26 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 
 function App() {
- 
+
   return (
     <>
-    <Navbar/>
-      <div className='container mx-auto my-5 p-5'>
-         <div className="">
-          <h1>My Tasks</h1>
-         </div>
+      <Navbar />
+      <div className='container mx-auto bg-violet-100 rounded-xl my-5 p-5 min-h-[80vh]'>
+        <div className="addTodo my-5">
+          <h2 className='text-lg font-bold'>Add a Todo</h2>
+          <input type="text" className='w-2/5' />
+          <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-6'>Add</button>
+        </div>
+        <h2 className='text-lg font-bold'>My Tasks</h2>
+        <div className="todos">
+          <div className="todo flex">
+            <div className="text">Lorem ipsum dolor sit amet.</div>
+            <div className="buttons">
+              <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-1'>Edit</button>
+              <button className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-1'>Remove</button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
