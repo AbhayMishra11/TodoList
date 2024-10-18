81a4e1e03622488514b8ddc9239861b2139d1d5f
AbhayMishra11 <55750241+AbhayMishra11@users.noreply.github.com>
2024-10-18 12:53:34 +0530

    Adding background img and other..

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="Navbar flex justify-between  p-5 bg-slate-800 text-white">
+    <nav className="Navbar flex justify-between  p-5 bg-transparent backdrop-blur-[5px] text-white border-b border-gray-800">
       <div className="logo font-bold mx-9 text-xl">Let's Go</div>
       <ul className='flex gap-[20px] mx-9'>
         <li className='cursor-pointer p-2 text-lg hover:font-bold transition-all duration-400'>Home</li>
