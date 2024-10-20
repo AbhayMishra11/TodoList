d661f447c994489998526ca8a36895824c9d03ea
AbhayMishra11 <55750241+AbhayMishra11@users.noreply.github.com>
2024-10-20 17:32:50 +0530

    completing my todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import { v4 as uuidv4 } from 'uuid';
+import { FaEdit } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
 
 function App() {
   const [todo, setTodo] = useState("")
@@ -66,26 +68,26 @@ function App() {
     <>
       <Navbar />
       <div className='container mx-auto bg-violet-100 rounded-xl my-5 p-5 min-h-[80vh] w-full lg:w-1/2'>
-      <h1 className='font-bold text-xl text-center'>Noto Your Daily Tasks Here</h1>
+      <h1 className='font-bold text-xl text-center'>Let's Go</h1>
         <div className="addTodo my-5 flex flex-col gap-4">
           <h2 className='text-lg font-bold text-center'>Add a Todo</h2>
-          <input type="text" className='w-full placeholder:text-black placeholder:text-bold placeholder:text-center hover:border border-blue-800'  placeholder='Enter Task Here' onChange={handleChange} value={todo} />
+          <input type="text" className='w-full placeholder:text-black placeholder:text-bold placeholder:text-center hover:border border-blue-800 rounded-lg py-1 px-3'  placeholder='Enter Task Here' onChange={handleChange} value={todo} />
           <button onClick={handleAdd} disabled={todo.length<3}  className='disabled:bg-violet-500 bg-violet-800 hover:bg-violet-950 p-2 rounded-md text-sm text-white w-full'>Add</button>
         </div>
         <input type="checkbox" onChange={showHide} checked={showFinshed}/> Show Finished
-        <h2 className='text-lg font-bold'>My Tasks</h2>
+        <h2 className='text-lg font-bold max-lg:text-center my-2'>My Tasks</h2>
         <div className="todos">
           {todos.length === 0 && <div>No todos to show</div>}
           {todos.map(items => {
-            return (showFinshed || !items.isCompleted) && <div key={items.id} className="todo flex w-full   lg:w-1/2 justify-between my-3">
+            return (showFinshed || !items.isCompleted) && <div key={items.id} className="todo flex w-full lg:w-1/2 justify-between my-3">
               <div className='flex gap-5'>
                 <input className='' type="checkbox" checked={items.isCompleted} onChange={toggleCheck} name={items.id} id={items.id} />
                 <label htmlFor={items.id}>
-                  <div className={items.isCompleted ? "line-through" : ""}>{items.todo}</div>
+                  <div className={items.isCompleted ? "line-through break-all" : "break-all"}>{items.todo}</div>
                 </label></div>
               <div className="buttons flex h-full">
-                <button onClick={(e) => { handleEdit(e, items.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-1'>Edit</button>
-                <button onClick={(e) => { handleDelete(e, items.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-1'>Remove</button>
+                <button onClick={(e) => { handleEdit(e, items.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-1'><FaEdit /></button>
+                <button onClick={(e) => { handleDelete(e, items.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-md text-white mx-1'><MdDeleteOutline /></button>
               </div>
             </div>
           })}
