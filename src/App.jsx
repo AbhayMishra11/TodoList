42f2422806563ae8b01dc7e2c66cb4be01550aa9
AbhayMishra11 <55750241+AbhayMishra11@users.noreply.github.com>
2024-10-20 16:56:05 +0530

    Adding responsive design to todopage

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   const handleAdd = () => {
       setTodos([...todos, { todo, id: uuidv4(), isCompleted: false }])
       setTodo("")
-      saveToLS(todos)
+      saveToLS([...todos, { todo, id: uuidv4(), isCompleted: false }])
   }
 
   const handleEdit = (e, id) => {
@@ -65,24 +65,25 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className='container mx-auto bg-violet-100 rounded-xl my-5 p-5 min-h-[80vh]'>
-        <div className="addTodo my-5">
-          <h2 className='text-lg font-bold'>Add a Todo</h2>
-          <input type="text" className='w-2/5' onChange={handleChange} value={todo} />
-          <button onClick={handleAdd} disabled={todo.length<3}  className='disabled:bg-violet-500 bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-6'>Add</button>
+      <div className='container mx-auto bg-violet-100 rounded-xl my-5 p-5 min-h-[80vh] w-full lg:w-1/2'>
+      <h1 className='font-bold text-xl text-center'>Noto Your Daily Tasks Here</h1>
+        <div className="addTodo my-5 flex flex-col gap-4">
+          <h2 className='text-lg font-bold text-center'>Add a Todo</h2>
+          <input type="text" className='w-full placeholder:text-black placeholder:text-bold placeholder:text-center hover:border border-blue-800'  placeholder='Enter Task Here' onChange={handleChange} value={todo} />
+          <button onClick={handleAdd} disabled={todo.length<3}  className='disabled:bg-violet-500 bg-violet-800 hover:bg-violet-950 p-2 rounded-md text-sm text-white w-full'>Add</button>
         </div>
         <input type="checkbox" onChange={showHide} checked={showFinshed}/> Show Finished
         <h2 className='text-lg font-bold'>My Tasks</h2>
         <div className="todos">
           {todos.length === 0 && <div>No todos to show</div>}
           {todos.map(items => {
-            return (showFinshed || !items.isCompleted) && <div key={items.id} className="todo flex w-[45%] justify-between my-3">
+            return (showFinshed || !items.isCompleted) && <div key={items.id} className="todo flex w-full   lg:w-1/2 justify-between my-3">
               <div className='flex gap-5'>
                 <input className='' type="checkbox" checked={items.isCompleted} onChange={toggleCheck} name={items.id} id={items.id} />
                 <label htmlFor={items.id}>
                   <div className={items.isCompleted ? "line-through" : ""}>{items.todo}</div>
                 </label></div>
-              <div className="buttons">
+              <div className="buttons flex h-full">
                 <button onClick={(e) => { handleEdit(e, items.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-1'>Edit</button>
                 <button onClick={(e) => { handleDelete(e, items.id) }} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 rounded-md text-sm text-white mx-1'>Remove</button>
               </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className="Navbar flex justify-between  p-5 bg-blue-700 text-white border-b border-gray-800">
-      <div className="logo font-bold mx-9 text-xl">Let's Go</div>
-      <ul className='flex gap-[20px] mx-9'>
+      <div className="logo font-bold mt-2 lg:mx-9 text-xl">Let's Go</div>
+      <ul className='flex lg:gap-[20px] lg:mx-9'>
         <li className='cursor-pointer p-2 text-lg hover:font-bold transition-all '>Home</li>
         <li className='cursor-pointer p-2 text-lg hover:font-bold  transition-all '>My Tasks</li>
       </ul>
